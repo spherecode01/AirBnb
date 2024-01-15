1a8cafd4370cec8d53156264d2bf7a9e46efcf07
simran <simranag951@gmail.com>
2024-01-15 12:24:10 +0530

    now LG

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -39,7 +39,7 @@ export default function RegisterPage() {
                  onChange={ev => setPassword(ev.target.value)} />
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
-            Already a member? <Link className="underline text-black" to={'/login'}>Login Button Testing</Link>
+            Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
           </div>
         </form>
       </div>
