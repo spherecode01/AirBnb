77de043edf2cf2e79ef73ffbb385b4fe4d2b0a7c
simran <simranag951@gmail.com>
2024-01-24 15:01:48 +0530

    cors change

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-//app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.netlify.app' }));
+app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.netlify.app' }));
 /*app.use((req, res, next) => {
   res.set({
       "Access-Control-Allow-Origin": "*",
