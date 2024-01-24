a5f4712ae5d42052d9b4f5ef6e47152b7cade107
simran <simranag951@gmail.com>
2024-01-24 11:24:14 +0530

    index change

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.netlify.app' }));
+//app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.netlify.app' }));
 /*app.use((req, res, next) => {
   res.set({
       "Access-Control-Allow-Origin": "*",
@@ -34,6 +34,12 @@ app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.ne
 
   next();
 });*/
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://illustrious-kashata-687ded.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 console.log(process.env.MONGO_URL);
 //mongoose.connect(process.env.MONGO_URL);
