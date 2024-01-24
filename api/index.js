b8b7409541fdb30874b3c3f66fc19c7264bbc082
simran <simranag951@gmail.com>
2024-01-24 11:37:57 +0530

    p

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.netlify.app' }));
+//app.use(cors({ credentials: true, origin: 'https://illustrious-kashata-687ded.netlify.app' }));
 /*app.use((req, res, next) => {
   res.set({
       "Access-Control-Allow-Origin": "*",
